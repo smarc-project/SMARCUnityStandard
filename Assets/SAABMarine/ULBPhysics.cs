3123a2b890aa4b21ea024775b2b6502972f4dabe
Albin <ziboms@gmail.com>
2025-02-18 08:19:54 +0100

    automatic unity changes

--- Assets/SAABMarine/ULBPhysics.cs
+++ Assets/SAABMarine/ULBPhysics.cs
@@ -40,7 +40,7 @@ namespace DefaultNamespace
             var world_rot = mainBody.transform.rotation.eulerAngles; 
                 
             //Get and convert state vector from global to local reference point
-            var inverseTransformDirection = mainBody.transform.InverseTransformDirection(mainBody.velocity); // Local frame vel
+            var inverseTransformDirection = mainBody.transform.InverseTransformDirection(mainBody.linearVelocity); // Local frame vel
             var transformAngularVelocity = mainBody.transform.InverseTransformDirection(mainBody.angularVelocity); // Local frame angular vel (gives negative velocities)
                 
             // Convert angles, angular velocities and velocities to OSBS coordinate system
